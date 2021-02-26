490ba44f6c9196afaa6da7b0d150b65adea85c81
“vatikhile” <vaibhawatikhile@gmail.com>
2021-02-26 23:52:10 +0530

    refactor the code for add the in the dashboard

--- fundoo/src/Components/Dashboard/Dashboard.jsx
+++ fundoo/src/Components/Dashboard/Dashboard.jsx
@@ -3,93 +3,263 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
-import TextField from "@material-ui/core/TextField";
-
-import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import { TextField } from "@material-ui/core";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 import Drawer from "@material-ui/core/Drawer";
 import { Tooltip } from "@material-ui/core";
 import "./Dashboard.css";
-
-const thm = createMuiTheme({
-  overrides: {
-    MuiDrawer: {
-      paperAnchorLeft: {
-        top: 65,
-        width: 260,
-        background: "white",
-      },
-      paperAnchorDockedLeft: {
-        borderRight: "1px solid transparent",
-      },
-    },
-    MuiAppBar: {
-      colorPrimary: {
-        color: "black",
-        backgroundColor: "white",
-      },
-      root: {
-        left: "auto",
-      },
+import Logo from "../../Asset/keep_logo.png";
+import styled from "styled-components";
+const drawerWidth = 240;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    marginTop: 64,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    marginTop: 64,
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1,
     },
   },
-});
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    backgroundColor: "white",
+
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  PrivateNotchedOutline: {
+    root: {
+      borderWidth: "0",
+    },
+  },
+}));
+const StyledTextField = styled(TextField)`
+  label.Mui-focused {
+    color: green;
+  }
+  .MuiOutlinedInput-root {
+    fieldset {
+      border-color: white;
+    }
+    &:hover fieldset {
+      border-color: white;
+    }
+    &.Mui-focused fieldset {
+      border-color: white;
+    }
+  }
+`;
+
 export default function PersistentDrawerLeft() {
+  const classes = useStyles();
+  // const mainClasses=useStyles2()
+  // const theme = useTheme();
   // const [age, setAge] = useState(42)
   // }
   // const [fruit, setFruit] = useState('banana');
   // const [todos, setTodos] = useState('[{ text: Learn Hooks }]');
 
   const [open, setOpen] = useState(true);
-
-  const handleToggle = () => {
-    // let toggle=!open
+  const handleDrawerOpen = () => {
     setOpen(!open);
-    console.log("demo", open);
   };
 
   return (
-    <div>
-      <MuiThemeProvider theme={thm}>
-        <AppBar position="fixed">
-          <Toolbar className="toolBar">
-            <Tooltip title="Main menu">
-              <IconButton
-                color="inherit"
-                aria-label="Open drawer"
-                onClick={handleToggle}
-              >
-                <MenuIcon id="menu" />
-              </IconButton>
-            </Tooltip>
-            {/* <div>{todos}</div> */}
-            <div className="menuBar">
-              <div>Keep</div>
-              <div>
-                <TextField
-                  placeholder="Search"
-                  variant="outlined"
-                  className="searchField"
-                  margin="normal"
-                />
-              </div>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar className="toolbar-content" position="fixed">
+        <Toolbar>
+          <Tooltip title="Main menu">
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={handleDrawerOpen}
+            >
+              <MenuIcon id="menu" />
+            </IconButton>
+          </Tooltip>
+          <img src={Logo} alt="FundooImg" style={{ width: "38px" }} />
+          <div className="menuBar">
+            <div>FundooNotes</div>
+            <div>
+              <StyledTextField
+                placeholder="Search"
+                variant="outlined"
+                className="searchField"
+                margin="normal"
+              />
             </div>
-          </Toolbar>
-        </AppBar>
-        <Drawer open={open} variant="persistent" anchor="left">
-          <div className="mainDrawer">
-            <Button>Notes</Button>
-
-            <Button>Reminder</Button>
-
-            <Button>Edit Label</Button>
-
-            <Button>Archive</Button>
-
-            <Button>Trash</Button>
           </div>
-        </Drawer>
-      </MuiThemeProvider>
+        </Toolbar>
+        <Divider />
+      </AppBar>
+
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <Divider />
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </main>
     </div>
   );
 }
+// <div>
+//   <MuiThemeProvider theme={thm}>
+//     <AppBar position="fixed">
+//       <Toolbar className="toolBar">
+//         <Tooltip title="Main menu">
+//           <IconButton
+//             color="inherit"
+//             aria-label="Open drawer"
+//             onClick={handleToggle}
+//           >
+//             <MenuIcon id="menu" />
+//           </IconButton>
+//         </Tooltip>
+//         {/* <div>{todos}</div> */}
+//         <div className="menuBar">
+//           <div>Keep</div>
+//           <div>
+//             <TextField
+//               placeholder="Search"
+//               variant="outlined"
+//               className="searchField"
+//               margin="normal"
+//             />
+//           </div>
+//         </div>
+//       </Toolbar>
+//     </AppBar>
+//     <Drawer open={open} variant="persistent" anchor="left">
+//       <div className="mainDrawer">
+//         <Button>Notes</Button>
+
+//         <Button>Reminder</Button>
+
+//         <Button>Edit Label</Button>
+
+//         <Button>Archive</Button>
+
+//         <Button>Trash</Button>
+//       </div>
+//     </Drawer>
+//   </MuiThemeProvider>
+// </div>
